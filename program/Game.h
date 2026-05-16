566beccf99671d7d24a9b80517952de6cab40de1
504-40\cre <a>
2026-05-16 19:13:59 +0900

    DxLibテンプレート

---
+++ program/Game.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define SCREEN_W	800
+#define SCREEN_H	450
+
+void GameInit();	//	‰Šú‰»ˆ—
+void GameUpdate();	//	XVˆ—
+void GameRender();	//	•`‰æˆ—
+void GameExit();	//	I—¹ˆ—
