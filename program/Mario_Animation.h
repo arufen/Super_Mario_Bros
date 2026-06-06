9a8b72fe965c34310120fb36e6025393519e5042
504-40\cre <a>
2026-06-06 17:31:53 +0900

    Mario_Animation.hのコードを更新して入れます

--- program/Mario_Animation.h
+++ program/Mario_Animation.h
@@ -1,10 +1,25 @@
+//========================================================
+//! マリオのアニメーション管理クラス
+//======================================================
+#pragma once
 #ifndef __MARIO_ANIMATION_H__
 #define __MARIO_ANIMATION_H__
 
+#include "HelpfulFunc.h"
 
+class MarioAnimation
+{
+public:
+	Animation walkAnimation;
 
+	// 初期化
+	void Init();
 
+	// アニメーションの更新
+	void Update(float now_speed_x, int mario_screenX, int mario_screenY);
 
+	// 描画
+	void Render(bool isLeft);
+};
 
-
-#endif
+#endif // __MARIO_ANIMATION_H__
